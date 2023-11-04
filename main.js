17a65fe8ac60fe0e4bbbed96442504d50a15abca
Sterling Smith <sterling12311@gmail.com>
2023-11-04 12:55:40 -0600

    inital commit

---
+++ main.js
@@ -0,0 +1,31 @@
+import './style.css'
+import javascriptLogo from './javascript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.js'
+import dayjs from 'dayjs'
+
+console.log(dayjs().format('MMM D, YYYY'));
+
+const a = dayjs("2023-09-05");
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+    </a>
+    <h1>Hello Vite!</h1>
+    <h2>Today's date: ${dayjs().format('MMM D, YYYY')}</h2>
+    <h2>${dayjs()}.toNow()</h2>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite logo to learn more
+    </p>
+  </div>
+`
+
+setupCounter(document.querySelector('#counter'))
